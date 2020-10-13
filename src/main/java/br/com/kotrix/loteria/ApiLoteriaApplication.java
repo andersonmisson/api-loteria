155880cf09c87a7bb9586248c7cf0f65e69b3c33
Anderson Misson <missondeveloper@gmail.com>
2020-10-13 17:31:36 -0300

    Iniciando o Projeto

---
+++ src/main/java/br/com/kotrix/loteria/ApiLoteriaApplication.java
@@ -0,0 +1,14 @@
+package br.com.kotrix.loteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLoteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLoteriaApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/br/com/kotrix/loteria/ApiLoteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kotrix.loteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLoteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
